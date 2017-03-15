502c2045ccdf9d9a66914b79197c69c81d18fec4
Konrad Gos <goskonrad@outlook.com>
2017-03-15 23:56:04 +0100

    finshed tests for Purchase class

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/purchase/PurchaseTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/purchase/PurchaseTest.java
@@ -1,5 +1,6 @@
 package pl.com.bottega.ecommerce.sales.domain.purchase;
 
+
 import org.junit.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -29,32 +30,52 @@ public class PurchaseTest {
 
     @Test
     public void isPaid() throws Exception {
+        List<PurchaseItem> items = null;
+        ClientData clientData = new ClientData(new Id("02"), "Adam");
+        Money money = new Money(12.4);
+        Purchase purchase = new Purchase(new Id("01"), clientData, items, new Date(2017,03,15),false, money);
 
+        assertThat(purchase.isPaid(), is(false));
     }
 
     @Test
     public void getTotalCost() throws Exception {
+        List<PurchaseItem> items = null;
+        ClientData clientData = new ClientData(new Id("02"), "Adam");
+        Money money = new Money(12.4);
+        Purchase purchase = new Purchase(new Id("01"), clientData, items, new Date(2017,03,15),false, money);
 
+        assertThat(purchase.getTotalCost(), is(money));
     }
 
     @Test
     public void getPurchaseDate() throws Exception {
+        List<PurchaseItem> items = null;
+        ClientData clientData = new ClientData(new Id("02"), "Adam");
+        Money money = new Money(12.4);
+        Purchase purchase = new Purchase(new Id("01"), clientData, items, new Date(2017,03,15),false, money);
 
+        assertThat(purchase.getPurchaseDate(), is(new Date(2017,03,15)));
     }
 
     @Test
     public void getClientData() throws Exception {
+        List<PurchaseItem> items = null;
+        ClientData clientData = new ClientData(new Id("02"), "Adam");
+        Money money = new Money(12.4);
+        Purchase purchase = new Purchase(new Id("01"), clientData, items, new Date(2017,03,15),false, money);
 
-    }
-
-    @Test
-    public void getItems() throws Exception {
-
+        assertThat(purchase.getClientData(), is(clientData));
     }
 
     @Test
     public void getAggregateId() throws Exception {
+        List<PurchaseItem> items = null;
+        ClientData clientData = new ClientData(new Id("02"), "Adam");
+        Money money = new Money(12.4);
+        Purchase purchase = new Purchase(new Id("01"), clientData, items, new Date(2017,03,15),false, money);
 
+        assertThat(purchase.getAggregateId(), is(new Id("01")));
     }
 
 }
