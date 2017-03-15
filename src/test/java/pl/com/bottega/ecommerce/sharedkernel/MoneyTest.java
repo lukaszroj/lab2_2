9fb3aa152c1f9e6e6d05f4f51daec4897ebff729
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-15 23:49:45 +0100

    Added Money test class, added test case for multiplying money by positive number

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,19 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by grusz on 15.03.2017.
+ */
+public class MoneyTest {
+    @Test public void multiplyByPositive () {
+        Money money = new Money(100);
+        Money expected = new Money(200);
+        Money result = money.multiplyBy(2);
+        Assert.assertThat(result,is(equalTo(expected)));
+    }
+}
