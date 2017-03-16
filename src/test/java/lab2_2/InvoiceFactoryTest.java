653aecfd05d189b8e293589d196481ded3ecc524
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-16 19:16:02 +0100

    InvoiceFactory test case created - checking for correct instance

---
+++ src/test/java/lab2_2/InvoiceFactoryTest.java
@@ -0,0 +1,22 @@
+package lab2_2;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+
+public class InvoiceFactoryTest {
+
+	@Test
+	public void checkIfCorrectReturnTypeFromFactory() {
+		InvoiceFactory ifact = new InvoiceFactory();
+		Invoice reality = ifact.create(new ClientData(Id.generate(), "Mateusz"));
+		assertThat(reality, is(instanceOf(Invoice.class)));
+	}
+
+}
