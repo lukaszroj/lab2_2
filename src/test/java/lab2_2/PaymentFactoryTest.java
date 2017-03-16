19f877194b92bfcd104b067a910e8038e8ebe851
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-16 19:13:50 +0100

    Payment Factory test case created - checking for correct instance

---
+++ src/test/java/lab2_2/PaymentFactoryTest.java
@@ -0,0 +1,23 @@
+package lab2_2;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.payment.Payment;
+import pl.com.bottega.ecommerce.sales.domain.payment.PaymentFactory;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentFactoryTest {
+
+	@Test
+	public void checkIfCorrectReturnTypeFromFactory() {
+		PaymentFactory pf = new PaymentFactory();
+		Payment reality = pf.createPayment(new ClientData(Id.generate(), "Mateusz"), new Money(123));
+		assertThat(reality, is(instanceOf(Payment.class)));
+	}
+
+}
