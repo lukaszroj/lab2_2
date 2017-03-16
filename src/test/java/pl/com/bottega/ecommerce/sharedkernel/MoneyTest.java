927acc4148ae8477124db52f1cb5589ce76e3704
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-16 19:56:20 +0100

    Dodanie testu dla metody multiplyBy z klasy Money [przypadek dla liczb dodatnich]

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Piotrek on 16.03.2017.
+ */
+public class MoneyTest {
+    @Test
+    public void multiplyByPositiveNumber() throws Exception {
+        Money money = new Money(150);
+        Money moneyExpected = new Money(300);
+
+        Money result = money.multiplyBy(2);
+        Assert.assertThat(result, is(equalTo(moneyExpected)));
+    }
+
+}
