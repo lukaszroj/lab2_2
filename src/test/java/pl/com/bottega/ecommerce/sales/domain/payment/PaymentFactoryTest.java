6ab6e2fa6d6357c89572537c8198963e0728a76b
Robert <ropak@wp.pl>
2017-03-16 18:21:36 +0100

    + Added test method for checking if PaymentFactory is not returning null

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
@@ -1,18 +1,29 @@
 package pl.com.bottega.ecommerce.sales.domain.payment;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class PaymentFactoryTest {
 
     private ClientData clientData;
+    private PaymentFactory factory;
 
     @Before
     public void setUp() throws Exception {
         clientData = new ClientData(Id.generate(), "John Doe");
+        factory = new PaymentFactory();
     }
 
+    @Test
+    public void testCreate_NotNull() throws Exception {
+        Payment payment = factory.createPayment(clientData, new Money(15));
+        assertThat(payment, notNullValue());
+    }
 }
