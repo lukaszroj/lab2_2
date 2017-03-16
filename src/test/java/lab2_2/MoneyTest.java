d361682578223f81a55a8c41f47d5bafd9d8195d
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-16 18:40:10 +0100

    Money test case added - multiply by positive (double)

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -24,4 +24,11 @@ public class MoneyTest {
 		Money reality = new Money(123);
 		assertThat(reality.multiplyBy(new BigDecimal(0.0)), is(equalTo(expectation)));
 	}
+	
+	@Test
+	public void multiplyByPositiveDouble() {
+		Money expectation = new Money(40);
+		Money reality = new Money(20);
+		assertThat(reality.multiplyBy(2.0), is(equalTo(expectation)));
+	}
 }
