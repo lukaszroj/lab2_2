4015814ccaaf2737f3ea004d7a1710c15e2da2b5
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-16 21:29:35 +0100

    Dodanie testu dla metody lessThen z klasy Money [dla dodatnich argumentów (NIE PRZECHODZI)]

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -159,4 +159,13 @@ public class MoneyTest {
         boolean result = money.greaterThan(moneyAdd);
         Assert.assertThat(result, is(true));
     }
+
+    @Test
+    public void lessThanPositiveNumbers(){
+        Money money = new Money(150);
+        Money moneyAdd = new Money(100);
+
+        boolean result = money.lessThan(moneyAdd);
+        Assert.assertThat(result, is(true));
+    }
 }
