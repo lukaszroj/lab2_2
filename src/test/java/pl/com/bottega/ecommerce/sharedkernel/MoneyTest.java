8eb4519a6163718593e0e38554a5c167c511d4ac
Konrad Gos <goskonrad@outlook.com>
2017-03-16 23:55:26 +0100

    added multiply tests in MoneyTest

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -2,12 +2,20 @@ package pl.com.bottega.ecommerce.sharedkernel;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by Konrad Gos on 16.03.2017.
  */
 public class MoneyTest {
-    
+    @Test
+    public void multiplyBy() {
+        Money money = new Money(10);
+        Money money1 = new Money(40);
+        Money moneyResult = money.multiplyBy(4);
+
+        assertThat(moneyResult, is(money1));
+    }
 
 }
