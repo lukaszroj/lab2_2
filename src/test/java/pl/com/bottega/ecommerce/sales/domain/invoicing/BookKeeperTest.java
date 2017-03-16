3bbb91d5fb98c3ba72d778642ab26c31a9cf261c
Robert <ropak@wp.pl>
2017-03-16 17:01:56 +0100

    * Updated BookKeeperTest setUp

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -2,16 +2,31 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import org.junit.Before;
 import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import static org.junit.Assert.*;
 
 public class BookKeeperTest {
 
     private BookKeeper keeper;
+    private TaxPolicy policy;
+    private ClientData clientData;
+
+    private class MockTaxPolicy implements TaxPolicy {
+
+        public Tax calculateTax(ProductType productType, Money net) {
+            return new Tax(net.multiplyBy(0.2), "MockTax");
+        }
+    }
 
     @Before
     public void setUp() throws Exception {
         keeper = new BookKeeper();
+        policy = new MockTaxPolicy();
+        clientData = new ClientData(Id.generate(), "John Doe");
     }
 
     @Test
