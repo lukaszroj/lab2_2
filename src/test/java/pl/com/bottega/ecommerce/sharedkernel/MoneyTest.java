0c6375016cb2a4616ada641a17abc6467c8428c1
Konrad Gos <goskonrad@outlook.com>
2017-03-16 23:49:40 +0100

    added empty test class for Money class - need to do the tests

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Konrad Gos on 16.03.2017.
+ */
+public class MoneyTest {
+    
+
+}
