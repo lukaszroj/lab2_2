8269fda3716fe4a35dd18f356d8019c1fb83115e
Robert <ropak@wp.pl>
2017-03-16 16:52:39 +0100

    + Added BookKeeperTest class and setUp method

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,22 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookKeeperTest {
+
+    private BookKeeper keeper;
+
+    @Before
+    public void setUp() throws Exception {
+        keeper = new BookKeeper();
+    }
+
+    @Test
+    public void issuance() throws Exception {
+
+    }
+
+}
