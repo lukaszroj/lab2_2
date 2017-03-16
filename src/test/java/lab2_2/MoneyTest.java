07119d73e128f80dc8a3a8b8604abc3a9824908f
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-16 18:40:51 +0100

    Money test case added - multiply by positive (BigDecimal)

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -31,4 +31,11 @@ public class MoneyTest {
 		Money reality = new Money(20);
 		assertThat(reality.multiplyBy(2.0), is(equalTo(expectation)));
 	}
+	
+	@Test
+	public void multiplyByPositiveBigDecimal() {
+		Money expectation = new Money(40);
+		Money reality = new Money(20);
+		assertThat(reality.multiplyBy(new BigDecimal(2.0)), is(equalTo(expectation)));
+	}
 }
