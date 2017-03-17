df69174a842f1335f0ca988f51b62f7431729482
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-17 14:18:51 +0100

    Dodanie testu do klasy InvoiceFactory metody create()

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Piotrek on 17.03.2017.
+ */
+public class InvoiceFactoryTest {
+    @Test
+    public void createMethodTest() throws Exception {
+
+    }
+
+}
