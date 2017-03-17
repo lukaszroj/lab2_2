dcf9c1f79ef520401cd1a48fd91b9a12a0ca5fe6
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-17 14:29:17 +0100

    Dodanie testu dla metody create z klasy PaymentFactory [dodanie porównania (NIE PRZECHODZI)]

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
@@ -1,10 +1,13 @@
 package pl.com.bottega.ecommerce.sales.domain.payment;
 
+import org.junit.Assert;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +20,9 @@ public class PaymentFactoryTest {
         Id id = Id.generate();
         ClientData clientData = new ClientData(Id.generate(), "TestUser1");
         Money money = new Money(150);
+
+        Payment result = paymentFactory.createPayment(clientData, money);
+        Assert.assertThat(result, is(instanceOf(PaymentFactory.class)));
     }
 
 }
