09c5fa8a40e304953a347d63d5c11928b5a56c3d
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-17 14:27:16 +0100

    Dodanie testu dla metody create z klasy PaymentFactory [dodanie potrzebnych konstruktorów]

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
@@ -0,0 +1,22 @@
+package pl.com.bottega.ecommerce.sales.domain.payment;
+
+import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Piotrek on 17.03.2017.
+ */
+public class PaymentFactoryTest {
+    @Test
+    public void createPaymentMethodTest() throws Exception {
+        PaymentFactory paymentFactory = new PaymentFactory();
+        Id id = Id.generate();
+        ClientData clientData = new ClientData(Id.generate(), "TestUser1");
+        Money money = new Money(150);
+    }
+
+}
