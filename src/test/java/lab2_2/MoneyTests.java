e498ebcadb4f279a3eba6bd5a0d44aec20fb1877
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-03-18 11:26:35 +0100

    Lab 2.2 testMultiplyByDouble

---
+++ src/test/java/lab2_2/MoneyTests.java
@@ -0,0 +1,42 @@
+package lab2_2;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import org.hamcrest.core.IsNot;
+import org.junit.Assert;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class MoneyTests {
+
+	@Test
+	public void testMultiplyByPositiveDouble() {
+		Money money = new Money(10);
+		Money expectedResult = new Money(100);
+		Assert.assertThat(money.multiplyBy(10), is(equalTo(expectedResult)));		
+	}	
+	@Test
+	public void testMultiplyByNegativeDouble() {
+		Money money = new Money(10);
+		Money expectedResult = new Money(-100);
+		Assert.assertThat(money.multiplyBy(-10), is(equalTo(expectedResult)));		
+	}	
+	@Test
+	public void testMultiplyByWithSameCurrencyDouble() {
+		Money money = new Money(10, Currency.getInstance("PLN"));
+		Money expectedResult = new Money(100, Currency.getInstance("PLN"));
+		Assert.assertThat(money.multiplyBy(10), is(equalTo(expectedResult)));		
+	}
+	
+	@Test
+	public void testMultiplyByZeroDouble() {
+		Money money = new Money(10);
+		Money expectedResult = new Money(0);
+		Assert.assertThat(money.multiplyBy(0), is(equalTo(expectedResult)));		
+	}
+}
