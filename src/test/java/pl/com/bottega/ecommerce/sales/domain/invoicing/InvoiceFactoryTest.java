526f8ab9694079603c0b72908b8d54d639e4d2f6
Konrad Gos <goskonrad@outlook.com>
2017-03-18 19:15:13 +0100

    added InvoiceFactoryTest - testing create method

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Konrad Gos on 18.03.2017.
+ */
+public class InvoiceFactoryTest {
+    @Test
+    public void testClientCreate() throws Exception {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        ClientData clientData = new ClientData(new Id("02"), "Adam");
+        Object invoice = invoiceFactory.create(clientData);
+
+        assertThat(invoice, is(instanceOf(Invoice.class)));
+    }
+
+}
