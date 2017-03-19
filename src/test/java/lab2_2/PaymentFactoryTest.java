7161a32951c4fc8d8f1ec192ab4b412954669756
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-19 18:10:50 +0100

    Utworzenie klasy testowej PaymentFactoryTest + test createPayment

---
+++ src/test/java/lab2_2/PaymentFactoryTest.java
@@ -0,0 +1,26 @@
+package lab2_2;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.payment.Payment;
+import pl.com.bottega.ecommerce.sales.domain.payment.PaymentFactory;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentFactoryTest {
+
+	@Test
+	public void testCreatePayment() {
+		
+		PaymentFactory paymentFactory = new PaymentFactory();
+		ClientData clientData = new ClientData(new Id("194972"), "Aleksander Kaczmarczyk");
+		Payment payment = paymentFactory.createPayment(clientData, new Money(100));
+		
+		Assert.assertThat(payment, notNullValue());
+	}
+
+}
