6d52f64b5d7042ee417a51776693f1cc41265dce
patrykxk <patrix215@gmail.com>
2017-03-19 13:10:02 +0100

    MoneyTests - poprawa testu domyślnej waluty

--- src/test/java/MoneyTests.java
+++ src/test/java/MoneyTests.java
@@ -1,9 +1,13 @@
 import org.junit.Assert;
+import org.junit.Test;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import java.util.Currency;
+
 public class MoneyTests {
+    @Test
     public void isDefaultCurrencyEUR(){
         Money money = new Money(100);
-        Assert.assertEquals("EUR", money.getCurrency());
+        Assert.assertEquals(Currency.getInstance("EUR"), money.getCurrency());
     }
 }
