e30f3cf57906ccfd01f7ba239042d6c015b6cbed
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-19 17:19:26 +0100

    MoneyTest - dodanie testu multiplyBy z argumentem double

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -1,14 +1,22 @@
 package lab2_2;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMultiplyByDouble() {
+		
+		Money money = new Money(5);
+		Money expectedMoney = new Money(20);
+		
+		Assert.assertThat(money.multiplyBy(4), is(equalTo(expectedMoney)));
 	}
 
 }
