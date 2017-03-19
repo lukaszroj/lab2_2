6f6bca1b7cf1f4b93f30fc41b0b001157dda5f1a
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-19 18:07:25 +0100

    Utworzenie klasy testowej PaymentTest + test rollBack

---
+++ src/test/java/lab2_2/PaymentTest.java
@@ -0,0 +1,28 @@
+package lab2_2;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.payment.Payment;
+import pl.com.bottega.ecommerce.sales.domain.payment.PaymentFactory;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentTest {
+
+	@Test
+	public void testRollBack() {
+		
+		PaymentFactory paymentFactory = new PaymentFactory();
+		ClientData clientData = new ClientData(new Id("194972"), "Aleksander Kaczmarczyk");
+		Payment payment = paymentFactory.createPayment(clientData, new Money(100));
+		Payment expectedPaymentAfterRollBack = paymentFactory.createPayment(clientData, new Money(-100));
+		
+		Payment paymentAfterRollBack = payment.rollBack();
+		Assert.assertThat(paymentAfterRollBack, is(equalTo(expectedPaymentAfterRollBack)));
+	}
+
+}
