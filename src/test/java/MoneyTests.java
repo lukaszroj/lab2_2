8984f0a015a8d17e6c2a9fdeb7b27cc01a2a4d22
patrykxk <patrix215@gmail.com>
2017-03-19 13:26:44 +0100

    MoneyTests dodano globalna zmienna

--- src/test/java/MoneyTests.java
+++ src/test/java/MoneyTests.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -6,22 +7,20 @@ import java.math.BigDecimal;
 import java.util.Currency;
 
 public class MoneyTests {
-    @Test
-    public void isDefaultCurrencyEUR(){
-        Money money = new Money(100);
-        Assert.assertEquals(Currency.getInstance("EUR"), money.getCurrency());
-    }
+    Money MONEY = new Money(new BigDecimal("100"));
 
     @Test
+    public void isDefaultCurrencyEUR(){
+        Assert.assertEquals(Currency.getInstance("EUR"), MONEY.getCurrency());
+    }
+    @Test
     public void multiplyByBigDecimalMultiplier(){
-        Money money = new Money(new BigDecimal("100"));
-        Money actual = money.multiplyBy(new BigDecimal("10"));
+        Money actual = MONEY.multiplyBy(new BigDecimal("10"));
         Assert.assertEquals(new Money(new BigDecimal("1000")),actual);
     }
     @Test
     public void multiplyByDoubleMultiplier(){
-        Money money = new Money(new BigDecimal("100"));
-        Money actual = money.multiplyBy(10.0);
+        Money actual = MONEY.multiplyBy(10.0);
         Assert.assertEquals(new Money(new BigDecimal("1000")),actual);
     }
 }
