6a9bc372ed5825f430e883db56fb6ecda7016170
patrykxk <patrix215@gmail.com>
2017-03-19 13:23:12 +0100

    MoneyTests test metody multiplyBy

--- src/test/java/MoneyTests.java
+++ src/test/java/MoneyTests.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import java.math.BigDecimal;
 import java.util.Currency;
 
 public class MoneyTests {
@@ -9,5 +10,18 @@ public class MoneyTests {
     public void isDefaultCurrencyEUR(){
         Money money = new Money(100);
         Assert.assertEquals(Currency.getInstance("EUR"), money.getCurrency());
+    }
+
+    @Test
+    public void multiplyByBigDecimalMultiplier(){
+        Money money = new Money(new BigDecimal("100"));
+        Money actual = money.multiplyBy(new BigDecimal("10"));
+        Assert.assertEquals(new Money(new BigDecimal("1000")),actual);
+    }
+    @Test
+    public void multiplyByDoubleMultiplier(){
+        Money money = new Money(new BigDecimal("100"));
+        Money actual = money.multiplyBy(10.0);
+        Assert.assertEquals(new Money(new BigDecimal("1000")),actual);
     }
 }
