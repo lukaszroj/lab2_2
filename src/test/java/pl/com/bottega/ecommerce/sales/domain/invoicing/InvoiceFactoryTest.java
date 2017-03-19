6acbe5450bed1321f9baec9cca28ff7df69dea8e
Klaudia Głuszek <klaudia.gluszek@gmail.com>
2017-03-20 00:34:39 +0100

    dodanie testów InvoiceFactory

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -0,0 +1,29 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Klaudia on 20.03.2017.
+ */
+public class InvoiceFactoryTest {
+    private ClientData clientData;
+    @Before
+    public void setUp() throws Exception {
+        clientData = new ClientData(Id.generate(), "Linus Torvalds");
+    }
+
+    @Test
+    public void create() throws Exception {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        Invoice result = invoiceFactory.create(clientData);
+        assertThat(result, is(instanceOf(Invoice.class)));
+    }
+
+}
