5b86e22ab4f671f675532bd677f9d510ef16e8e4
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-19 18:18:21 +0100

    Utworzenie klasy testowej InvoiceFactoryTest + test create

---
+++ src/test/java/lab2_2/InvoiceFactoryTest.java
@@ -0,0 +1,25 @@
+package lab2_2;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+
+public class InvoiceFactoryTest {
+
+	@Test
+	public void testCreate() {
+		
+		InvoiceFactory invoiceFactory = new InvoiceFactory();
+		ClientData clientData = new ClientData(new Id("194972"), "Aleksander Kaczmarczyk");
+		
+		Invoice invoice = invoiceFactory.create(clientData);
+		Assert.assertThat(invoice, notNullValue());
+	}
+
+}
