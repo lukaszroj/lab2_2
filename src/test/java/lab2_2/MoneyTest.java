76805d34232b3c13ab8cea832544cf64a3d7bd88
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-03-20 11:00:09 +0100

    Utworzenie testu metody multiplyBy klasy Money

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -1,14 +1,23 @@
 package lab2_2;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMultiplyByMethod() {
+		Money result = new Money(100);
+		final double multiplier = 4;
+		final Money testValue = new Money(400);
+		
+		result = result.multiplyBy(multiplier);
+		
+		assertThat(result, is(testValue));
 	}
 
 }
