52d83fc8ecb50423413395d88395ee842913b236
ciupa.kamil <194928@edu.p.lodz.pl>
2017-03-20 21:33:01 +0100

    MoneyTest) Dodano test dla metody multiplyBy

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -4,7 +4,20 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 
 public class MoneyTest {
 
+	final Money money = new Money(50);
+	
+	@Test
+	public void testMultiplyByDoubleMultiplier() {
+		
+		Money expectedMoney = new Money(100);
+		double multiplier = 2.0;
+		Money actualMoney = money.multiplyBy(multiplier);
+		
+		Assert.assertThat(actualMoney, is(expectedMoney));
+	}
 }
