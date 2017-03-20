4c3aa547034f411645f965f620cdba69174adf5f
Patron <patroneq@gmail.com>
2017-03-20 20:15:43 +0100

    test metody add w przypadku tej samej waluty i roznej waluty

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/Test/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/Test/MoneyTest.java
@@ -17,5 +17,28 @@ public class MoneyTest {
          assertThat(multiply, equalTo(new Money(100, "PLN")));
          assertThat(multiply.getCurrencyCode(), equalTo("PLN"));
      }
-
+	 
+	 @Test
+     public void testAddCase1() {
+         Money money1 = new Money(5);
+         Money money2 = new Money(25);
+ 
+         Money sumOfMoney = money1.add(money2);
+ 
+         Money result = new Money(30);
+ 
+         assertThat(sumOfMoney, equalTo(result));
+     }
+	 
+	 @Test (expected = IllegalArgumentException.class)
+     public void testAddCase2() throws IllegalArgumentException {
+         Money money1 = new Money(5, "PLN");
+         Money money2 = new Money(25, "USD");
+ 
+         Money sumOfMoney = money1.add(money2);
+ 
+         Money result = new Money(30);
+ 
+         assertThat(sumOfMoney, equalTo(result));
+     }
 }
