c39b3d4154700267fdd87298390bdd631d9b30c6
Jakub Stawiszyński <kubastyk@wp.pl>
2017-03-20 22:15:07 +0100

    MoneyTest - add money test

--- src/test/java/pl/test/MoneyTest.java
+++ src/test/java/pl/test/MoneyTest.java
@@ -18,4 +18,13 @@ public class MoneyTest {
 		assertThat(money.multiplyBy(multiplier), is(equalTo(new Money(111))));
 	}
 
+	@Test
+	public void addMoneyTest() {
+
+		Money money = new Money(22.2);
+		Money money2 = new Money(27.8);
+
+		assertThat(money.add(money2), is(equalTo(new Money(50))));
+	}
+
 }
