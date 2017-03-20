f5c60529550b6dfb2c077b038cd53d0e2b7a0971
Patron <patroneq@gmail.com>
2017-03-20 20:08:00 +0100

    test metody multiplyBy klasy Money

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/Test/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/Test/MoneyTest.java
@@ -3,12 +3,19 @@ package pl.com.bottega.ecommerce.sharedkernel.Test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	 @Test
+     public void testMultiplyBy() throws Exception {
+         Money money = new Money(10, "PLN");
+ 
+         Money multiply = money.multiplyBy(10);
+ 
+         assertThat(multiply, equalTo(new Money(100, "PLN")));
+         assertThat(multiply.getCurrencyCode(), equalTo("PLN"));
+     }
 
 }
