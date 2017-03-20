a0f2a47de9dd559a26e1ab9bb960d8b658f2ea6c
ciupa.kamil <194928@edu.p.lodz.pl>
2017-03-20 23:05:58 +0100

    IdTest) Dodanie testu dla metody generate

--- src/test/java/lab2_2/IdTest.java
+++ src/test/java/lab2_2/IdTest.java
@@ -6,6 +6,17 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
 public class IdTest {
 
+	@Test
+	public void testGenerate() {
+		
+		Id firstId = Id.generate();
+		Id secondId = Id.generate();
+		
+		Assert.assertThat(firstId, is(not(secondId)));
+	}
+	
 }
