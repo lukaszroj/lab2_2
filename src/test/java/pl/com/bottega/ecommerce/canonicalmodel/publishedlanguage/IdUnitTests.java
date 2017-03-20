073c94063b0c3c163af8bdd7033a86fee8fc1918
KhaledWielki <adam.rezner95@gmail.com>
2017-03-20 21:09:05 +0100

    Id class. Added 2 tests for 'equals' metod. Chceck if 2 Id's are the same, and different.

--- src/test/java/pl/com/bottega/ecommerce/canonicalmodel/publishedlanguage/IdUnitTests.java
+++ src/test/java/pl/com/bottega/ecommerce/canonicalmodel/publishedlanguage/IdUnitTests.java
@@ -11,8 +11,24 @@ import org.junit.Test;
 public class IdUnitTests {
 
 	@Test
-	public void generate() throws Exception{
+	public void generateTest() throws Exception{
 		Id id = new Id(Id.generate().toString());
 		assertThat(id, is(notNullValue()));
+	}
+	
+	@Test
+	public void hashCodeTestAreIdsAreDifferent() throws Exception{
+		Id testId1 = new Id("1");
+		Id testId2 = new Id("2");
+		
+		assertThat(testId1.equals(testId2), is(false));
+	}
+	
+	@Test
+	public void hashCodeTestAreIdsAreTheSame() throws Exception{
+		Id testId1 = new Id("1");
+		Id testId2 = new Id("1");
+		
+		assertThat(testId1.equals(testId2), is(true));
 	}
 }
