6922be88865cda7b6e7d8075e8ee90da3091bcce
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-20 15:10:32 +0100

    create Money test class & empty test methods

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,56 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pebuls on 20.03.17.
+ */
+public class MoneyTest {
+    @Test
+    public void multiplyBy() throws Exception {
+
+    }
+
+    @Test
+    public void multiplyBy1() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void subtract() throws Exception {
+
+    }
+
+    @Test
+    public void getCurrencyCode() throws Exception {
+
+    }
+
+    @Test
+    public void getCurrency() throws Exception {
+
+    }
+
+    @Test
+    public void greaterThan() throws Exception {
+
+    }
+
+    @Test
+    public void lessThan() throws Exception {
+
+    }
+
+    @Test
+    public void lessOrEquals() throws Exception {
+
+    }
+
+}
