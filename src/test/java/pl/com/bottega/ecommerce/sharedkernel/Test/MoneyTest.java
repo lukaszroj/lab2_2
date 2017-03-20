90ce029f17bac37557bf102bba2aa4bcf72f1a93
Patron <patroneq@gmail.com>
2017-03-20 20:03:03 +0100

    dodanie klasy MoneyTest bez implementacji zadnych metod

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/Test/MoneyTest.java
@@ -0,0 +1,14 @@
+package pl.com.bottega.ecommerce.sharedkernel.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
