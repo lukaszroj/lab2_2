3e419aa6c363a44e995f3cbf18b9e37d383face0
Klaudia Głuszek <klaudia.gluszek@gmail.com>
2017-03-20 09:04:35 +0100

    dodanie testów BookKeeper

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -18,7 +18,6 @@ import static org.junit.Assert.*;
  * Created by Klaudia on 20.03.2017.
  */
 public class BookKeeperTest {
-    private InvoiceFactory invoiceFactory = new InvoiceFactory();
 
     private ClientData clientData;
 
@@ -36,20 +35,18 @@ public class BookKeeperTest {
 
     @Test
     public void testIssuance() throws Exception {
+        BookKeeper bookKeeper = new BookKeeper();
         InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
         TaxPolicy tax = new MockTaxPolicy();
-
         ProductData productData = new ProductData(Id.generate(), new Money(10), "apple", ProductType.FOOD,
                 new Date());
         RequestItem requestItem = new RequestItem(productData, 2, productData.getPrice().multiplyBy(2));
-
         invoiceRequest.add(requestItem);
+        InvoiceLine expected = new InvoiceLine(productData, 2,new Money(20), tax.calculateTax(productData.getType(),requestItem.getTotalCost()));
 
-        BookKeeper bookKeeper = new BookKeeper();
         Invoice result = bookKeeper.issuance(invoiceRequest, tax);
-        Money expected = new Money(24);
-        assertThat(result.getGros(), is(expected));
 
+        assertThat(result.getItems().get(0), equalTo(expected));
 
     }
 
