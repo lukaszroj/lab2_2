c360724cda2782282a4706218980a9ff1df9da2b
Robert Jankowski <jankowski.r@hotmail.com>
2017-03-20 22:42:01 +0100

    Money test - refactor multiply Euro, add Dollars

--- src/test/java/tests/TestMoney.java
+++ src/test/java/tests/TestMoney.java
@@ -1,11 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties. To change this template file, choose
+ * Tools | Templates and open the template in the editor.
  */
 package tests;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 import org.hamcrest.CoreMatchers;
 import static org.hamcrest.core.Is.is;
 import org.junit.Assert;
@@ -18,24 +18,38 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
  * @author jankowskirobert
  */
 public class TestMoney {
-    
+
     private final Money EUR10 = new Money(10, "EUR");
     private final Money EUR15 = new Money(15, "EUR");
     private final Money EUR21 = new Money(21, "EUR");
     private final Money USD13 = new Money(13, "USD");
     private final Money USD18 = new Money(18, "USD");
     private final Money USD27 = new Money(27, "USD");
-    
+
     @Test
-    public void testMultiplyEuro(){
+    public void testMultiplyEuro() {
         final Money eur10By15 = new Money(150, "EUR");
         assertThat(EUR10.multiplyBy(15), is(eur10By15));
-        
-        final Money eur15By15 = new Money(225, "EUR");
+
+        final Money eur15By15 = new Money(225, Currency.getInstance("EUR"));
         assertThat(EUR15.multiplyBy(15), is(eur15By15));
-        
-        final Money eur21by15 = new Money(315, "EUR");
+
+        final Money eur21by15 = new Money(new BigDecimal(315), Currency.getInstance("EUR"));
         assertThat(EUR21.multiplyBy(15), is(eur21by15));
-        
+
     }
+
+    @Test
+    public void testMultiplyDollar() {
+        final Money usd13By15 = new Money(195, "USD");
+        assertThat(USD13.multiplyBy(15), is(usd13By15));
+
+        final Money usd18By15 = new Money(new BigDecimal(270), Currency.getInstance("USD"));
+        assertThat(USD18.multiplyBy(15), is(usd18By15));
+
+        final Money usd27By15 = new Money(405, Currency.getInstance("USD"));
+        assertThat(USD27.multiplyBy(15), is(usd27By15));
+
+    }
+
 }
