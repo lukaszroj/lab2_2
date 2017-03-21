6f921337fd45fc501c94897d4b7ae6330baea103
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 13:06:09 +0100

    Zastąpienie klasy MoneyTest klasą Junit MoneyTest

--- src/test/java/pl/lab2_2/test/MoneyTest.java
+++ src/test/java/pl/lab2_2/test/MoneyTest.java
@@ -1,5 +1,14 @@
 package pl.lab2_2.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class MoneyTest {
-	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
 }
