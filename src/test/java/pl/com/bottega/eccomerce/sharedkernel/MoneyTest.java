1fc7498a8260ac38b84f519dfa7827cad867b939
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-21 11:40:41 +0100

    Test subtractin Money

--- src/test/java/pl/com/bottega/eccomerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/eccomerce/sharedkernel/MoneyTest.java
@@ -20,4 +20,14 @@ public class MoneyTest {
         Money result = entryValue.add(moneyToAdd);
         assertThat(result, is(expectedMoney));
     }
+
+    @Test
+    public void testSubtractMoney() {
+        final Money entryValue = new Money(500);
+        final Money moneyToSubtract = new Money(200);
+        final Money expectedMoney = new Money(300);
+
+        Money result = entryValue.subtract(moneyToSubtract);
+        assertThat(result, is(expectedMoney));
+    }
 }
