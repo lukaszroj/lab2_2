e3af52e9be09a560807c0e082036f0ef5fa44ef0
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 13:37:34 +0100

    Utworzenie pustej klasy Junit PaymentTest

---
+++ src/test/java/pl/lab2_2/test/PaymentTest.java
@@ -0,0 +1,14 @@
+package pl.lab2_2.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaymentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
