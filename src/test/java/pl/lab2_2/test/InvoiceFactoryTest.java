5de95be2e1e818878d65d2bc65f7f44f7994b82d
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 13:28:02 +0100

    Dodanie testu sprawdzającego typ tworzony przez InvoiceFactory

--- src/test/java/pl/lab2_2/test/InvoiceFactoryTest.java
+++ src/test/java/pl/lab2_2/test/InvoiceFactoryTest.java
@@ -1,14 +1,25 @@
 package pl.lab2_2.test;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
 
 public class InvoiceFactoryTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createInvoiceHasExpectedType() {
+		
+		InvoiceFactory invoiceFactory = new InvoiceFactory();
+		ClientData clientData = new ClientData(Id.generate(), "testowa");
+		Object object = invoiceFactory.create(clientData);
+		assertThat(object, is(instanceOf(Invoice.class)));
 	}
 
 }
