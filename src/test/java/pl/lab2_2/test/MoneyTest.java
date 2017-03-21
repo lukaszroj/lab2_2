20f673abbdd70b212f22259c6b7e2de54be76efd
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 13:09:57 +0100

    Dodanie pierwszego testu klasy Money i aktualizacja importów

--- src/test/java/pl/lab2_2/test/MoneyTest.java
+++ src/test/java/pl/lab2_2/test/MoneyTest.java
@@ -1,14 +1,22 @@
 package pl.lab2_2.test;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMultiplyByPositive() {
+		
+		Money money = new Money(30);
+		Money expected = new Money(90);
+		Money result = money.multiplyBy(3);
+		assertThat(result, is(equalTo(expected)));
 	}
 
 }
