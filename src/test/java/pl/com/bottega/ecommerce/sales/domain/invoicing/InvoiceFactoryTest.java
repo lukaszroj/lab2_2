e9d0c11d861a80fd89310f36856a7405190d74b2
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-21 14:59:59 +0100

    remowing unused imports in InvoiceFatroryTets

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -1,13 +1,8 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
-
 import static org.hamcrest.CoreMatchers.*;
-
 import org.junit.Test;
-
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 
