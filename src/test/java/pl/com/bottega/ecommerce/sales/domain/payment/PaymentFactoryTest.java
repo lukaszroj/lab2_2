fa069e1a9111efda4dd3aea1de5398622967dc5f
Przyjemski.Maciej <180264@edu.p.lodz.pl>
2017-03-21 14:33:23 +0100

    dodanie testu PaymentFactoryTest

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
@@ -0,0 +1,27 @@
+package pl.com.bottega.ecommerce.sales.domain.payment;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.payment.Payment;
+import pl.com.bottega.ecommerce.sales.domain.payment.PaymentFactory;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+
+public class PaymentFactoryTest {
+
+	@Test
+	public void createPayment_checkType() {
+		Id clientId = Id.generate();
+		Money money = new Money(100);
+		ClientData data = new ClientData(clientId, "Andrew");
+		PaymentFactory factory = new PaymentFactory();
+		Payment result = factory.createPayment(data, money);
+		Assert.assertThat(result, is(instanceOf(Payment.class)));
+	}
+
+}
