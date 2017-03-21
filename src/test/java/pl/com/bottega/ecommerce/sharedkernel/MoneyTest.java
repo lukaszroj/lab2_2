510488440a34ac741f3c989585bf02b5d31133e6
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-21 15:15:21 +0100

    multiplyBy big decimal test added

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -2,17 +2,29 @@ package pl.com.bottega.ecommerce.sharedkernel;
 
 import static org.hamcrest.CoreMatchers.*;
 
+import java.math.BigDecimal;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoneyTest {
 
 	@Test
-	public void testMultiplyBy() {
+	public void testMultiplyByDoubleMultiplier() {
 		Money money = new Money(100);
 		double multiplier = 5.0;
 		
 		Assert.assertThat(money.multiplyBy(multiplier), is(equalTo(new Money(500.0))));
 	}
+	
+	@Test
+	public void testMultiplyByBigDecimalMultiplier() {
+		Money money = new Money(100);
+		BigDecimal multiplier = new BigDecimal("6.0");
+		
+		Assert.assertThat(money.multiplyBy(multiplier), is(equalTo(new Money(600.0))));
+	}
+	
+	
 
 }
