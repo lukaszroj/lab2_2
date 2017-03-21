fb01d6b2386ffc278ea5bf767214186d46138c81
195024 <195024@edu.p.lodz.pl>
2017-03-21 13:54:05 +0100

    Money class, multiplication keeps the same currency

--- src/test/java/lab2_2/TMoney.java
+++ src/test/java/lab2_2/TMoney.java
@@ -1,6 +1,7 @@
 package lab2_2;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -56,5 +57,21 @@ public class TMoney {
 		Money expectedResult = new Money(-6);
 
 		Assert.assertThat(money.multiplyBy(new BigDecimal(-2)), is(equalTo(expectedResult)));
+	}
+	
+	@Test
+	public void multiplyByDoubleKeepsTheSameCurrency() {
+		Money money = new Money(20, Currency.getInstance("GBP"));
+		Money expectedResult = new Money(40, Currency.getInstance("GBP"));
+		
+		Assert.assertThat(money.multiplyBy(2), is(equalTo(expectedResult)));
+	}
+
+	@Test
+	public void multiplyByBigDecimalKeepsTheSameCurrency() {
+		Money money = new Money(25, Currency.getInstance("USD"));
+		Money expectedResult = new Money(150, Currency.getInstance("USD"));
+		
+		Assert.assertThat(money.multiplyBy(new BigDecimal(6)), is(equalTo(expectedResult)));
 	}
 }
