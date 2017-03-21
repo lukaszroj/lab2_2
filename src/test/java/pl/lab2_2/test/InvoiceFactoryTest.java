e6133ad62e054234dbf589baa181afa8efd4ed22
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 13:24:46 +0100

    Dodanie pustej klasy Junit InvoiceFactoryTest

---
+++ src/test/java/pl/lab2_2/test/InvoiceFactoryTest.java
@@ -0,0 +1,14 @@
+package pl.lab2_2.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InvoiceFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
