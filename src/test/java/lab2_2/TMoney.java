385a746e166b01d686c2a67ab14fdae492bfc4b8
195024 <195024@edu.p.lodz.pl>
2017-03-21 14:30:48 +0100

    Money class, getCurrencyCode() tests

--- src/test/java/lab2_2/TMoney.java
+++ src/test/java/lab2_2/TMoney.java
@@ -3,6 +3,7 @@ package lab2_2;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import org.hamcrest.core.IsNot;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
@@ -170,61 +171,74 @@ public class TMoney {
 
 		Assert.assertThat(money.subtract(new Money(19, Currency.getInstance("KRW"))), is(equalTo(expectedResult)));
 	}
-	
+
 	@Test
 	public void greaterThanMethodReturnsTrue() {
 		Money money = new Money(87);
 		Money testMoney = new Money(86);
-		
+
 		Assert.assertThat(money.greaterThan(testMoney), is(true));
 	}
-	
+
 	@Test
 	public void greaterThanMethodReturnsFalse() {
 		Money money = new Money(86);
 		Money testMoney = new Money(87);
-		
+
 		Assert.assertThat(money.greaterThan(testMoney), is(false));
 	}
-	
+
 	@Test
 	public void lessThanMethodReturnsTrue() {
 		Money money = new Money(87);
 		Money testMoney = new Money(86);
-		
+
 		Assert.assertThat(money.lessThan(testMoney), is(false));
 	}
-	
+
 	@Test
 	public void lessThanMethodReturnsFalse() {
 		Money money = new Money(86);
 		Money testMoney = new Money(87);
-		
+
 		Assert.assertThat(money.lessThan(testMoney), is(true));
 	}
-	
+
 	@Test
 	public void lessOrEqualsMethodReturnsTrueWhenLess() {
 		Money money = new Money(123);
 		Money testMoney = new Money(321);
-		
+
 		Assert.assertThat(money.lessOrEquals(testMoney), is(true));
 	}
-	
+
 	@Test
 	public void lessOrEqualsMethodReturnsTrueWhenEqual() {
 		Money money = new Money(99);
 		Money testMoney = new Money(99);
-		
+
 		Assert.assertThat(money.lessOrEquals(testMoney), is(true));
 	}
-	
+
 	@Test
 	public void lessOrEqualsMethodReturnsFalseWhenGreater() {
 		Money money = new Money(60000);
 		Money testMoney = new Money(32);
-		
+
 		Assert.assertThat(money.lessOrEquals(testMoney), is(false));
 	}
+
+	@Test
+	public void getCurrencyCodeIsEqual() {
+		Money money = new Money(552, Currency.getInstance("PLN"));
+		
+		Assert.assertThat(money.getCurrencyCode(), is(equalTo(Currency.getInstance("PLN").getCurrencyCode())));
+	}
 	
+	@Test
+	public void getCurrencyCodeIsNotEqual() {
+		Money money = new Money(525, Currency.getInstance("PLN"));
+		
+		Assert.assertThat(money.getCurrencyCode(), IsNot.not(equalTo(Currency.getInstance("KRW").getCurrencyCode())));
+	}
 }
