52119b418f2a1c5d8a66c58cc51e0b83537f20ec
195024 <195024@edu.p.lodz.pl>
2017-03-21 13:49:22 +0100

    Money class, multiplication by zero

---
+++ src/test/java/lab2_2/TMoney.java
@@ -0,0 +1,29 @@
+package lab2_2;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class TMoney {
+
+	@Test
+	public void multiplyByZeroDouble() {
+		Money money = new Money(5);
+		Money expectedResult = new Money(0);
+
+		Assert.assertThat(money.multiplyBy(0), is(equalTo(expectedResult)));
+	}
+
+	@Test
+	public void multiplyByZeroBigDecimal() {
+		Money money = new Money(10);
+		Money expectedResult = new Money(0);
+
+		Assert.assertThat(money.multiplyBy(new BigDecimal(0)), is(equalTo(expectedResult)));
+	}
+
+}
