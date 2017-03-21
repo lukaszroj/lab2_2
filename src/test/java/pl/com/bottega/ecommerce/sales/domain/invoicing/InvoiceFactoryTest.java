871d3ddfab56ea237818621927cbd73694b2d037
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-21 14:53:25 +0100

    InvoiceFatrory test

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+public class InvoiceFactoryTest {
+	
+	@Test
+	public void testInvoiceFactory() {
+		ClientData client = new ClientData(Id.generate(), "Test");
+		InvoiceFactory invoiceFactory = new InvoiceFactory();
+		Assert.assertThat(invoiceFactory.create(client), instanceOf(Invoice.class));
+	}
+
+}
