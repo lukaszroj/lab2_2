2da9eac1c141baa69187a0a2115263ca6efd1738
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-23 14:08:29 +0100

    add test for PaymentFactoryTest class: createPaymentCheckIsNotNull

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryTest.java
@@ -1,7 +1,11 @@
 package pl.com.bottega.ecommerce.sales.domain.payment;
 
 import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -9,7 +13,17 @@ import static org.junit.Assert.*;
  */
 public class PaymentFactoryTest {
 
+    private PaymentFactory paymentFactory;
+    private Payment payment;
 
 
+    @Test
+    public void createPaymentCheckIsNotNull() throws Exception {
+
+        paymentFactory = new PaymentFactory();
+
+        payment = paymentFactory.createPayment(new ClientData(Id.generate(), "Name Surname"), new Money(15));
+        assertThat(payment, notNullValue());
+    }
 
 }
