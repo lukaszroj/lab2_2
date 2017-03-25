a6cdea428ca9a3887442887c0767f5b585742d57
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 13:40:16 +0100

    PaymentFactoryUnitTests class added

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryUnitTests.java
@@ -0,0 +1,14 @@
+package pl.com.bottega.ecommerce.sales.domain.payment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaymentFactoryUnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
