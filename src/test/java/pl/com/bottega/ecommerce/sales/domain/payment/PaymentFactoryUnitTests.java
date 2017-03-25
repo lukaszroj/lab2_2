7e6076eefe5ce0dc94d252b537534b90a5927f90
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 13:42:48 +0100

    Some imports added to PaymentFactoryUnitTests

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryUnitTests.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactoryUnitTests.java
@@ -2,15 +2,22 @@ package pl.com.bottega.ecommerce.sales.domain.payment;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class PaymentFactoryUnitTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+		/*@Test
+		public void isCreatePaymentCorrectyType() {
+		ClientData clientData = new ClientData(Id.generate(), "Test");
+		PaymentFactory factory = new PaymentFactory();
+		Payment result = factory.createPayment(clientData, new Money(1000));
+	
+	    assertThat(result, is(instanceOf(Payment.class)));    
+	}*/
 }
