9a0ed99146e7bb0a3697d5c7758f2b71c60d8ddd
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 13:36:42 +0100

    Inports added to InvoiceFactoryTests

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryUnitTests.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryUnitTests.java
@@ -1,6 +1,13 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 
 import org.junit.Test;
 
