271f73f44e4fdcbe23e9af16c0e7a2438d685782
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-04-25 00:55:08 +0200

    Utworzenie klasy InvoiceFactoryTest, dodanie testu createInvoiceTest
    Signed-off-by: zimny.krzysztof <180318@edu.p.lodz.pl>

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactoryTest.java
@@ -0,0 +1,34 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Created by Krzysztof Zimny on 4/25/2017.
+ */
+public class InvoiceFactoryTest {
+
+    private ClientData clientData;
+
+    @Before
+    private void createInitialData() {
+        clientData = createClientDataInstance();
+    }
+
+    private ClientData createClientDataInstance() {
+        return new ClientData(Id.generate(), "Krzysztof Zimny");
+    }
+
+    @Test
+    private void createInvoiceTest() {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        Invoice invoice = invoiceFactory.create(clientData);
+        Assert.assertThat(invoice, is(instanceOf(Invoice.class)));
+    }
+}
