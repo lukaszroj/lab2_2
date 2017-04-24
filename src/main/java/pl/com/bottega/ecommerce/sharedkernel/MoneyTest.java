e0db6b33fe02f2b8164ae1969e7d4a3a131c2ccc
Khandree <a.lis.marciniak@gmail.com>
2017-04-24 21:52:14 +0200

    Created test addSameCurrency

--- src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -11,8 +11,17 @@ public class MoneyTest {
     @Test
     public void multiplyBy() throws Exception {
         Money money = new Money(50, Currency.getInstance("PLN"));
-            Money result = money.multiplyBy(2);
-            assertThat(result.getCurrency(), equalTo(money.getCurrency()));
-            assertThat(result, equalTo(new Money(100, money.getCurrency())));
+        Money result = money.multiplyBy(2);
+        assertThat(result.getCurrency(), equalTo(money.getCurrency()));
+        assertThat(result, equalTo(new Money(100, money.getCurrency())));
+    }
+
+    @Test
+    public void addSameCurrency() throws Exception {
+        Money money = new Money(75, Currency.getInstance("PLN"));
+        Money result = new Money(25, Currency.getInstance("PLN"));
+        Money actual = money.add(result);
+        assertThat(actual.getCurrency(), equalTo(money.getCurrency()));
+        assertThat(actual, equalTo(new Money(100, money.getCurrency())));
     }
 }
